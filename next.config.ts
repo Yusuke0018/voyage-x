03ab934df1deb7be96d364fff6819953645b1074
大岩祐介 <YUSUKE@MacBook-Air.local>
2025-08-22 12:21:21 +0900

    GitHub Pages\u30c7\u30d7\u30ed\u30a4\u8a2d\u5b9a\u3092\u30b7\u30f3\u30d7\u30eb\u5316

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/voyage' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/voyage' : '',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 };
 
 export default nextConfig;
